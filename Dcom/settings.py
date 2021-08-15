a390a821d48406d1dbc8e8654a03826ffcb3f8de
samsonatere <samsonatere24@gmail.com.com>
2021-08-15 14:43:09 +0100

    SK

--- Dcom/settings.py
+++ Dcom/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = 'ppYe9V-Ti7rOyC9TGn93Mrr5ADSStnk1xNEBfN8J_G4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
