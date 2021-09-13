c52907e2180a5dec00e98f1c6ccaca632b428f9a
samsonatere <samsonatere24@gmail.com.com>
2021-09-13 21:14:51 +0100

    Adjustment

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from . import views
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/', views.signupPage, name='signup'),
+    path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,52 @@
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from .form import CreateUserForm
+from .decorators import unauthenticated_user #allowed_users, admin_only
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+
+@unauthenticated_user
+def signupPage(request):
+
+	form = CreateUserForm()
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+
+			group = Group.objects.get(name='customer')
+			user.groups.add(group)
+
+			messages.success(request, 'Account was created for ' + username)
+
+			return redirect('login')
+		
+
+	context = {'form':form}
+	return render(request, 'registration/signup.html', context)
+
+@unauthenticated_user
+def loginPage(request):
+
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password =request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('store')
+		else:
+			messages.info(request, 'Username OR password is incorrect')
+
+	context = {}
+	return render(request, 'registration/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
